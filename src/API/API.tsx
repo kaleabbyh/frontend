702a83cdcc4df25190aba870f9aa5a95e8aff4bc
kaleabbyh <kaleabbyh@gmail.com>
2024-02-20 12:25:44 +0300

    update: API/API.tsx response type refactored

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { SongType } from "../state/SongType";
-const baseUrl = "http://localhost:8000/api";
+const baseUrl = "http://localhost:8080/api";
 
-export const getAllSongs = (): Promise<AxiosResponse<SongType[]>> => {
-  return axios.get<SongType[]>(`${baseUrl}/getAllSongs`);
+export const getAllSongs = (): Promise<SongType[]> => {
+  return axios.get(`${baseUrl}/getAllSongs`);
 };
 
-export const addSong = (song: SongType): Promise<AxiosResponse<SongType>> => {
-  return axios.post<SongType>(`${baseUrl}/addSong`, song);
+export const addSong = (song: SongType): Promise<SongType> => {
+  return axios.post(`${baseUrl}/addSong`, song);
 };
 
-export const editSong = (song: SongType) => {
+export const editSong = (song: SongType): Promise<SongType> => {
   return axios.put(`${baseUrl}/editSong/${song._id}`, song);
 };
 
-export const deleteSong = (song: SongType): Promise<AxiosResponse<string>> => {
-  return axios.delete<string>(`${baseUrl}/deleteSong/${song._id}`);
+export const deleteSong = (song: SongType): Promise<string> => {
+  return axios.delete(`${baseUrl}/deleteSong/${song._id}`);
 };
